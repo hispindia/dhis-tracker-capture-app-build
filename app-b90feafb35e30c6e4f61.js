1d7691cc641fb93d036e58ace1313eb7fcc89076
Lukas Wijgaart van Dijk <lukasewvd@gmail.com>
2018-11-15 10:21:10 +0000

    02a5470 Changes to validation strategy

--- app-b90feafb35e30c6e4f61.js
+++ app-b90feafb35e30c6e4f61.js
@@ -14405,8 +14405,6 @@
 	    $scope.model.showEventSearch = false;
 	    $scope.model.eventSearchText = '';
 	
-	    $scope.completeClicked = false;
-	
 	    $scope.filterLegend = function () {
 	        if ($scope.mainMenuStageSelected()) {
 	            return { showInEventLegend: true };
@@ -15828,7 +15826,7 @@
 	            }
 	        }
 	
-	        if (field && field.$invalid && $scope.currentStage.validationStrategy === "ON_UPDATE_AND_INSERT") {
+	        if (field && field.$invalid) {
 	            $scope.currentEvent[prStDe.dataElement.id] = oldValue;
 	            $scope.currentElement = { id: prStDe.dataElement.id, saved: false, event: eventToSave.event };
 	            return false;
@@ -16236,7 +16234,6 @@
 	
 	        if ($scope.currentEvent.status !== 'COMPLETED') {
 	            $scope.outerDataEntryForm.submitted = true;
-	            $scope.completeClicked = true;
 	            if ($scope.outerDataEntryForm.$invalid) {
 	                NotificationService.showNotifcationDialog($translate.instant("error"), $translate.instant("form_invalid"));
 	                return;
@@ -16256,7 +16253,6 @@
 	                bodyText: 'are_you_sure_to_incomplete_event'
 	            };
 	            dhis2Event.status = 'ACTIVE';
-	            $scope.completeClicked = false;
 	        } else {
 	            //complete event    
 	            //We must execute the rules right before deciding wheter to allow completion:
@@ -38241,4 +38237,4 @@
 
 /***/ }
 /******/ ]);
-//# sourceMappingURL=app-721e9bee7de819468203.js.map
+//# sourceMappingURL=app-b90feafb35e30c6e4f61.js.map
