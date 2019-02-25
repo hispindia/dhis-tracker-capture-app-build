cf861063e2b67de66ca78c1be9ab761f890fca15
Lukas Wijgaart van Dijk <lukasewvd@gmail.com>
2019-02-25 13:09:29 +0000

    57d57ff v31.0.44

--- app-0f9e6c1cf4858a71f441.js
+++ app-0f9e6c1cf4858a71f441.js
@@ -1039,7 +1039,7 @@
 	.service('CustomFormService', function ($translate, NotificationService) {
 	
 	    return {
-	        getForProgramStage: function (programStage, programStageDataElements) {
+	        getForProgramStage: function (programStage, programStageDataElements, singleStageProgram) {
 	
 	            var htmlCode = programStage.dataEntryForm ? programStage.dataEntryForm.htmlCode : null;
 	            var timeFormat = "24h"
@@ -1113,6 +1113,7 @@
 	                                //check if dataelement has optionset
 	                                if (prStDe.dataElement.optionSetValue) {
 	                                    var optionSetId = prStDe.dataElement.optionSet.id;
+	                                    var optionFilter = singleStageProgram ? 'd2-option-filter="optionVisibility"' : 'd2-option-filter="optionVisibility[currentEvent.event]"';
 	                                    newInputField = '<span class="hideInPrint" ng-if="!isHidden(prStDes.' + fieldId + '.dataElement.id, currentEvent)">' +
 	                                                    '<d2-option-list d2-model="currentEvent"' + 
 	                                                    'd2-model-id="prStDes.' + fieldId + '.dataElement.id"' +
@@ -1120,7 +1121,7 @@
 	                                                    'd2-disabled="' + disableInputField + '"' +
 	                                                    'd2-change="saveDatavalue(prStDes.' + fieldId + ', outerForm.' + fieldId + ')"' +
 	                                                    'd2-max-option-size="maxOptionSize"' +
-	                                                    'd2-option-filter="optionVisibility"' +
+	                                                    optionFilter +
 	                                                    'd2-all-options="optionSets.' + optionSetId + '.options">' +
 	                                                    '</d2-option-list>' +
 	                                                    '</span>';
@@ -38440,4 +38441,4 @@
 
 /***/ }
 /******/ ]);
-//# sourceMappingURL=app-0dcc4add8db5ad58ce45.js.map
+//# sourceMappingURL=app-0f9e6c1cf4858a71f441.js.map
