1c5a1d290370b47acca328ef5b7acb99797fee2b
Lukas Wijgaart van Dijk <lukasewvd@gmail.com>
2019-02-28 09:25:46 +0000

    ac0d8f3 v29.0.70

--- app-b643b0f644c36e8b9b42.js
+++ app-b643b0f644c36e8b9b42.js
@@ -9165,6 +9165,7 @@
 	        var modalInstance = $modal.open({
 	            templateUrl: 'components/dataentry/new-event.html',
 	            controller: 'EventCreationController',
+	            windowClass: 'modal-new-event-window',
 	            resolve: {
 	                eventsByStage: function eventsByStage() {
 	                    return _eventsByStage;
@@ -37119,4 +37120,4 @@
 
 /***/ }
 /******/ ]);
-//# sourceMappingURL=app-7bcafb2540a1bae56543.js.map
+//# sourceMappingURL=app-b643b0f644c36e8b9b42.js.map
