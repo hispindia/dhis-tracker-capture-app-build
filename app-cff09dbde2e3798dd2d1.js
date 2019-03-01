d269d3c12ca7f70beedfa1e11311f6378d9b2cdd
Lukas Wijgaart van Dijk <lukasewvd@gmail.com>
2019-03-01 14:22:07 +0000

    b2536c0 Removed redundant api calls to trackedEntityInstances and enrollments

--- app-cff09dbde2e3798dd2d1.js
+++ app-cff09dbde2e3798dd2d1.js
@@ -19691,10 +19691,7 @@
 	                $scope.relationships[rel.id] = rel;
 	            });
 	
-	            TEIService.getRelationships($scope.selectedTei.trackedEntityInstance).then(function (relationships) {
-	                $scope.selectedTei.relationships = relationships;
-	                setRelationships();
-	            });
+	            setRelationships();
 	        });
 	        $scope.selectedOrgUnit = $scope.selections.orgUnit;
 	    });
@@ -20847,18 +20844,14 @@
 	        $scope.selectedTei = selections.tei;
 	
 	        if (selections.selectedEnrollment && selections.selectedEnrollment.enrollment) {
-	            EnrollmentService.get(selections.selectedEnrollment.enrollment).then(function (data) {
-	                if (data) {
-	                    $scope.selectedEnrollment = data;
-	                    if (!angular.isUndefined($scope.selectedEnrollment.notes)) {
-	                        $scope.selectedEnrollment.notes = orderByFilter($scope.selectedEnrollment.notes, '-storedDate');
-	                        angular.forEach($scope.selectedEnrollment.notes, function (note) {
-	                            note.displayDate = DateUtils.formatFromApiToUser(note.storedDate);
-	                            note.storedDate = DateUtils.formatToHrsMins(note.storedDate);
-	                        });
-	                    }
-	                }
-	            });
+	            $scope.selectedEnrollment = angular.copy(selections.selectedEnrollment);
+	            if (!angular.isUndefined($scope.selectedEnrollment.notes)) {
+	                $scope.selectedEnrollment.notes = orderByFilter($scope.selectedEnrollment.notes, '-storedDate');
+	                angular.forEach($scope.selectedEnrollment.notes, function (note) {
+	                    note.displayDate = DateUtils.formatFromApiToUser(note.storedDate);
+	                    note.storedDate = DateUtils.formatToHrsMins(note.storedDate);
+	                });
+	            }
 	        }
 	    });
 	
@@ -38455,4 +38448,4 @@
 
 /***/ }
 /******/ ]);
-//# sourceMappingURL=app-3007bf2c505d7324bc9f.js.map
+//# sourceMappingURL=app-cff09dbde2e3798dd2d1.js.map
