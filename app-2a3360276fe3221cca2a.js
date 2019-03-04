0905d7bd818a3adce227d2095089b2aea3609ca2
Lukas Wijgaart van Dijk <lukasewvd@gmail.com>
2019-03-04 08:22:15 +0000

    51bf10c v29.0.71

--- app-2a3360276fe3221cca2a.js
+++ app-2a3360276fe3221cca2a.js
@@ -18303,10 +18303,7 @@
 	                $scope.relationships[rel.id] = rel;
 	            });
 	
-	            TEIService.getRelationships($scope.selectedTei.trackedEntityInstance).then(function (relationships) {
-	                $scope.selectedTei.relationships = relationships;
-	                setRelationships();
-	            });
+	            setRelationships();
 	        });
 	        $scope.selectedOrgUnit = $scope.selections.orgUnit;
 	    });
@@ -19364,18 +19361,14 @@
 	        $scope.selectedTei = selections.tei;
 	
 	        if (selections.selectedEnrollment && selections.selectedEnrollment.enrollment) {
-	            EnrollmentService.get(selections.selectedEnrollment.enrollment).then(function (data) {
-	                if (data) {
-	                    $scope.selectedEnrollment = data;
-	                    if (!angular.isUndefined($scope.selectedEnrollment.notes)) {
-	                        $scope.selectedEnrollment.notes = orderByFilter($scope.selectedEnrollment.notes, '-storedDate');
-	                        angular.forEach($scope.selectedEnrollment.notes, function (note) {
-	                            note.displayDate = DateUtils.formatFromApiToUser(note.storedDate);
-	                            note.storedDate = DateUtils.formatToHrsMins(note.storedDate);
-	                        });
-	                    }
-	                }
-	            });
+	            $scope.selectedEnrollment = angular.copy(selections.selectedEnrollment);
+	            if (!angular.isUndefined($scope.selectedEnrollment.notes)) {
+	                $scope.selectedEnrollment.notes = orderByFilter($scope.selectedEnrollment.notes, '-storedDate');
+	                angular.forEach($scope.selectedEnrollment.notes, function (note) {
+	                    note.displayDate = DateUtils.formatFromApiToUser(note.storedDate);
+	                    note.storedDate = DateUtils.formatToHrsMins(note.storedDate);
+	                });
+	            }
 	        }
 	    });
 	
@@ -37120,4 +37113,4 @@
 
 /***/ }
 /******/ ]);
-//# sourceMappingURL=app-b643b0f644c36e8b9b42.js.map
+//# sourceMappingURL=app-2a3360276fe3221cca2a.js.map
