cc8d18c2ca00023d622540d9d88803182000d249
Lukas Wijgaart van Dijk <lukasewvd@gmail.com>
2019-03-14 14:21:49 +0000

    6f21d0d v31.0.53

--- app-a904a0b85f993177cc11.js
+++ app-a904a0b85f993177cc11.js
@@ -19780,32 +19780,35 @@
 	        $scope.relatedTeisFrom = [];
 	        $scope.relationshipPrograms = [];
 	        var relationshipProgram = {};
+	        var relationshipType = {};
 	        //Loop through all relationships.      
 	        angular.forEach($scope.selectedTei.relationships, function (rel) {
 	            if (rel.to && rel.to.trackedEntityInstance && rel.to.trackedEntityInstance.trackedEntityInstance !== $scope.selectedTei.trackedEntityInstance) {
 	                var teiId = rel.to.trackedEntityInstance.trackedEntityInstance;
 	                var relName = rel.relationshipName;
 	                TEIService.get(teiId, $scope.optionSets, $scope.attributesById).then(function (tei) {
-	                    relationshipProgram = $scope.relationshipTypes.find(function (relType) {
+	                    relationshipType = $scope.relationshipTypes.find(function (relType) {
 	                        return relType.id === rel.relationshipType;
-	                    }).toConstraint.program;
+	                    });
+	                    relationshipProgram = relationshipType.toConstraint.program;
 	                    if (!relationshipProgram && $scope.selectedProgram) {
 	                        relationshipProgram = { id: $scope.selectedProgram.id };
 	                    }
-	                    var relative = { trackedEntityInstance: teiId, relName: relName, relId: rel.relationship, attributes: getRelativeAttributes(tei.attributes), relationshipProgramConstraint: relationshipProgram };
+	                    var relative = { trackedEntityInstance: teiId, relName: relName, relId: rel.relationship, attributes: getRelativeAttributes(tei.attributes), relationshipProgramConstraint: relationshipProgram, relationshipType: relationshipType };
 	                    $scope.relatedTeisTo.push(relative);
 	                });
 	            } else if (rel.from && rel.from.trackedEntityInstance && rel.from.trackedEntityInstance.trackedEntityInstance !== $scope.selectedTei.trackedEntityInstance) {
 	                var teiId = rel.from.trackedEntityInstance.trackedEntityInstance;
 	                var relName = rel.relationshipName;
 	                TEIService.get(teiId, $scope.optionSets, $scope.attributesById).then(function (tei) {
-	                    relationshipProgram = $scope.relationshipTypes.find(function (relType) {
+	                    relationshipType = $scope.relationshipTypes.find(function (relType) {
 	                        return relType.id === rel.relationshipType;
-	                    }).fromConstraint.program;
+	                    });
+	                    relationshipProgram = relationshipType.toConstraint.program;
 	                    if (!relationshipProgram && $scope.selectedProgram) {
 	                        relationshipProgram = { id: $scope.selectedProgram.id };
 	                    }
-	                    var relative = { trackedEntityInstance: teiId, relName: relName, relId: rel.relationship, attributes: getRelativeAttributes(tei.attributes), relationshipProgramConstraint: relationshipProgram };
+	                    var relative = { trackedEntityInstance: teiId, relName: relName, relId: rel.relationship, attributes: getRelativeAttributes(tei.attributes), relationshipProgramConstraint: relationshipProgram, relationshipType: relationshipType };
 	                    $scope.relatedTeisFrom.push(relative);
 	                });
 	            }
@@ -19967,6 +19970,9 @@
 	
 	    $scope.today = DateUtils.getToday();
 	    $scope.relationshipTypes = relationshipTypes;
+	    $scope.relationshipTypesForSelector = relationshipTypes.filter(function (relationshipType) {
+	        return relationshipType.access.data.write;
+	    });
 	    $scope.addingRelationship = addingRelationship;
 	    $scope.selectedAttribute = selectedAttribute;
 	    $scope.selectedProgram = selectedProgram;
@@ -38451,4 +38457,4 @@
 
 /***/ }
 /******/ ]);
-//# sourceMappingURL=app-c23b8b42b97723defcb2.js.map
+//# sourceMappingURL=app-a904a0b85f993177cc11.js.map
