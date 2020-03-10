e28440200349d139dc60426edb237c83ff5d8a43
Joakim Storløkken Melseth <joakim.melseth@gmail.com>
2020-03-10 14:13:21 +0000

    fe22533 fix: DHIS2-8317 frontend updates - remove non-searching attributes from request - show only attributes with displayInList

--- app-3d77cb773a52c7b658f3.js
+++ app-3d77cb773a52c7b658f3.js
@@ -10662,7 +10662,8 @@
 	                attributes.push({
 	                    id: grid.headers[i].name,
 	                    displayName: grid.headers[i].column,
-	                    type: grid.headers[i].type
+	                    type: grid.headers[i].type,
+	                    hideInList: grid.headers[i].hideInList
 	                });
 	            }
 	
@@ -10718,7 +10719,9 @@
 	            });
 	
 	            var len = entityList.own.length + entityList.other.length;
-	            return { headers: attributes, rows: entityList, pager: grid.metaData.pager, length: len };
+	            return { headers: attributes.filter(function (a) {
+	                    return !a.hideInList;
+	                }), rows: entityList, pager: grid.metaData.pager, length: len };
 	        },
 	        generateGridColumns: function generateGridColumns(attributes, ouMode, nonConfidential) {
 	
@@ -11678,18 +11681,8 @@
 	        return true;
 	    };
 	
-	    this.programScopeSearch = function (programSearchGroup, tetSearchGroup, program, trackedEntityType, orgUnit, pager, sortColumn) {
-	        var params = getSearchParams(programSearchGroup, program, trackedEntityType, orgUnit, pager, searchScopes.PROGRAM, function (filteredAttributes) {
-	            var programAttributes = program.programTrackedEntityAttributes;
-	            return programAttributes.map(function (programAttribute) {
-	                return programAttribute.displayInList && programAttribute.trackedEntityAttribute && programAttribute.trackedEntityAttribute.id;
-	            }).filter(function (attributeId) {
-	                return attributeId && !filteredAttributes[attributeId];
-	            }).reduce(function (acc, attributeId) {
-	                acc += "&attribute=" + attributeId;
-	                return acc;
-	            }, '');
-	        });
+	    this.programScopeSearch = function (programSearchGroup, tetSearchGroup, program, trackedEntityType, orgUnit, pager, sortColumn, onEditHeadersFromReponse) {
+	        var params = getSearchParams(programSearchGroup, program, trackedEntityType, orgUnit, pager, searchScopes.PROGRAM);
 	
 	        if (params) {
 	            var programScopeFetchAsyncFn = function programScopeFetchAsyncFn(pager, sortColumn) {
@@ -11699,7 +11692,11 @@
 	
 	            return programScopeFetchAsyncFn(params.pager, sortColumn).then(function (response) {
 	                if (response && response.rows && response.rows.length > 0) {
+	                    if (onEditHeadersFromReponse) {
+	                        response.headers = onEditHeadersFromReponse(response.headers, program.programTrackedEntityAttributes);
+	                    }
 	                    var result = { data: response, callingScope: searchScopes.PROGRAM, resultScope: searchScopes.PROGRAM, onRefetch: programScopeFetchAsyncFn };
+	
 	                    var def = $q.defer();
 	                    if (params.uniqueSearch) {
 	                        result.status = "UNIQUE";
@@ -11710,7 +11707,7 @@
 	                    return def.promise;
 	                } else {
 	                    if (tetSearchGroup) {
-	                        return tetScopeSearch(tetSearchGroup, trackedEntityType, orgUnit, pager).then(function (result) {
+	                        return tetScopeSearch(tetSearchGroup, trackedEntityType, orgUnit, pager, undefined, onEditHeadersFromReponse).then(function (result) {
 	                            result.callingScope = searchScopes.PROGRAM;
 	                            return result;
 	                        }, function () {
@@ -11737,18 +11734,8 @@
 	            return def.promise;
 	        }
 	    };
-	    var tetScopeSearch = this.tetScopeSearch = function (tetSearchGroup, trackedEntityType, orgUnit, pager, sortColumn) {
-	        var params = getSearchParams(tetSearchGroup, null, trackedEntityType, orgUnit, pager, searchScopes.TET, function (filteredAttributes) {
-	            var tetAttributes = trackedEntityType.trackedEntityTypeAttributes;
-	            return tetAttributes.map(function (tetAttribute) {
-	                return tetAttribute.displayInList && tetAttribute.trackedEntityAttribute && tetAttribute.trackedEntityAttribute.id;
-	            }).filter(function (attributeId) {
-	                return attributeId && !filteredAttributes[attributeId];
-	            }).reduce(function (acc, attributeId) {
-	                acc += "&attribute=" + attributeId;
-	                return acc;
-	            }, '');
-	        });
+	    var tetScopeSearch = this.tetScopeSearch = function (tetSearchGroup, trackedEntityType, orgUnit, pager, sortColumn, onEditHeadersFromReponse) {
+	        var params = getSearchParams(tetSearchGroup, null, trackedEntityType, orgUnit, pager, searchScopes.TET);
 	        if (params) {
 	            var tetScopeFetchAsyncFn = function tetScopeFetchAsyncFn(pager, sortColumn) {
 	                var order = sortColumn && "order=" + sortColumn.id + ":" + sortColumn.direction;
@@ -11756,6 +11743,9 @@
 	            };
 	
 	            return tetScopeFetchAsyncFn(params.pager, sortColumn).then(function (response) {
+	                if (onEditHeadersFromReponse) {
+	                    response.headers = onEditHeadersFromReponse(response.headers, trackedEntityType.trackedEntityTypeAttributes);
+	                }
 	                var result = { data: response, callingScope: searchScopes.TET, resultScope: searchScopes.TET, onRefetch: tetScopeFetchAsyncFn };
 	                if (response && response.rows && response.rows.length > 0) {
 	                    if (params.uniqueSearch) {
@@ -15282,6 +15272,19 @@
 	    value: true
 	});
 	exports.processRegistration = processRegistration;
+	function showOnlyDisplayInListAttributes(headers, attributesContainer) {
+	    var attributeHeaders = headers.slice(7);
+	    attributeHeaders.forEach(function (attributeHeader) {
+	        var foundAttributeContainer = attributesContainer.find(function (attributeContainer) {
+	            return (attributeContainer.trackedEntityAttribute && attributeContainer.trackedEntityAttribute.id) === attributeHeader.name;
+	        });
+	        if (foundAttributeContainer && !foundAttributeContainer.displayInList) {
+	            attributeHeader.hideInList = true;
+	        }
+	    });
+	    return headers;
+	};
+	
 	function verifyUniqueSearchGroup(searchGroup, _ref) {
 	    var useProgramSearchScope = _ref.useProgramSearchScope,
 	        SearchGroupService = _ref.SearchGroupService,
@@ -15294,9 +15297,9 @@
 	    var promise;
 	    if (useProgramSearchScope) {
 	        var tetSearchGroup = SearchGroupService.findTetSearchGroup(searchGroup, tetSearchConfig);
-	        promise = SearchGroupService.programScopeSearch(searchGroup, tetSearchGroup, program, trackedEntityType, orgUnit);
+	        promise = SearchGroupService.programScopeSearch(searchGroup, tetSearchGroup, program, trackedEntityType, orgUnit, undefined, undefined, showOnlyDisplayInListAttributes);
 	    } else {
-	        promise = SearchGroupService.tetScopeSearch(searchGroup, trackedEntityType, orgUnit);
+	        promise = SearchGroupService.tetScopeSearch(searchGroup, trackedEntityType, orgUnit, undefined, undefined, showOnlyDisplayInListAttributes);
 	    }
 	
 	    return promise.then(function (res) {
@@ -15374,9 +15377,9 @@
 	
 	    var promise;
 	    if (useProgramSearchScope) {
-	        promise = SearchGroupService.programScopeSearch(defaultSearchGroup, tetSearchGroup, program, trackedEntityType, orgUnit, { skipTotalPages: true });
+	        promise = SearchGroupService.programScopeSearch(defaultSearchGroup, tetSearchGroup, program, trackedEntityType, orgUnit, { skipTotalPages: true }, undefined, showOnlyDisplayInListAttributes);
 	    } else {
-	        promise = SearchGroupService.tetScopeSearch(defaultSearchGroup, trackedEntityType, orgUnit, { skipTotalPages: true });
+	        promise = SearchGroupService.tetScopeSearch(defaultSearchGroup, trackedEntityType, orgUnit, { skipTotalPages: true }, undefined, showOnlyDisplayInListAttributes);
 	    }
 	
 	    return promise.then(function (res) {
@@ -23121,12 +23124,25 @@
 	                return;
 	            }
 	
+	            var showOnlyDisplayInListAttributes = function showOnlyDisplayInListAttributes(headers, attributesContainer) {
+	                var attributeHeaders = headers.slice(7);
+	                attributeHeaders.forEach(function (attributeHeader) {
+	                    var foundAttributeContainer = attributesContainer.find(function (attributeContainer) {
+	                        return (attributeContainer.trackedEntityAttribute && attributeContainer.trackedEntityAttribute.id) === attributeHeader.name;
+	                    });
+	                    if (foundAttributeContainer && !foundAttributeContainer.displayInList) {
+	                        attributeHeader.hideInList = true;
+	                    }
+	                });
+	                return headers;
+	            };
+	
 	            var promise;
 	            if (currentSearchScope === searchScopes.PROGRAM) {
 	                var tetSearchGroup = SearchGroupService.findValidTetSearchGroup(searchGroup, $scope.tetSearchConfig, $scope.base.attributesById);
-	                promise = SearchGroupService.programScopeSearch(searchGroup, tetSearchGroup, $scope.base.selectedProgram, $scope.trackedEntityTypes.selected, $scope.selectedOrgUnit, { skipTotalPages: true });
+	                promise = SearchGroupService.programScopeSearch(searchGroup, tetSearchGroup, $scope.base.selectedProgram, $scope.trackedEntityTypes.selected, $scope.selectedOrgUnit, { skipTotalPages: true }, undefined, showOnlyDisplayInListAttributes);
 	            } else {
-	                promise = SearchGroupService.tetScopeSearch(searchGroup, $scope.trackedEntityTypes.selected, $scope.selectedOrgUnit, { skipTotalPages: true });
+	                promise = SearchGroupService.tetScopeSearch(searchGroup, $scope.trackedEntityTypes.selected, $scope.selectedOrgUnit, { skipTotalPages: true }, undefined, showOnlyDisplayInListAttributes);
 	            }
 	
 	            return promise.then(function (res) {
@@ -39763,4 +39779,4 @@
 
 /***/ }
 /******/ ]);
-//# sourceMappingURL=app-da1f5b7c8a81f61259e5.js.map
+//# sourceMappingURL=app-3d77cb773a52c7b658f3.js.map
